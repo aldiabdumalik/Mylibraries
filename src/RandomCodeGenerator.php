1c7ac8b75c68a406068ca0f2308010c34ab4a2dc
aldiabdumalik <aldiabdumalik@hotmail.com>
2021-05-05 12:45:02 +0700

    first commit

---
+++ src/RandomCodeGenerator.php
@@ -0,0 +1,61 @@
+<?php
+namespace updu\Mylibraries;
+
+class RandomCodeGenerator {
+    
+    public function CharNumb($length = 10)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomCode = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomCode .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomCode;
+    }
+
+    public function Char($length = 10)
+    {
+        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomCode = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomCode .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomCode;
+    }
+
+    public function CharUppercase($length = 10)
+    {
+        $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZZYXWVUTSRQPONMLKJIHGFEDCA';
+        $charactersLength = strlen($characters);
+        $randomCode = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomCode .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomCode;
+    }
+
+    public function CharLowercase($length = 10)
+    {
+        $characters = 'abcdefghijklmnopqrstuvwxyzzyxwvutsrqponmlkjihgfedca';
+        $charactersLength = strlen($characters);
+        $randomCode = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomCode .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomCode;
+    }
+
+    public function Numb($length = 10)
+    {
+        $characters = '01234567899876543210132546798';
+        $charactersLength = strlen($characters);
+        $randomCode = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomCode .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomCode;
+    }
+
+}
